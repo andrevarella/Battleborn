8d0d3d3ba60dfc11fe652019daab5de7088bb80f
andrevarella <andre.varella@hotmail.com>
2023-06-21 05:50:25 -0300

    (Unit.cpp) Fix Conflagrate not Blizzlike Delay

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -4476,7 +4476,7 @@ void Unit::_ApplyAura(AuraApplication* aurApp, uint8 effMask)
         else if (caster)
         {
             ConflagrateAuraStateDelayEvent* pEvent = new ConflagrateAuraStateDelayEvent(this, caster->GetGUID());
-            m_Events.AddEvent(pEvent, m_Events.CalculateTime(700)); // intended 700ms delay before allowing to cast conflagrate
+            m_Events.AddEvent(pEvent, m_Events.CalculateTime(0)); // intended 700ms delay before allowing to cast conflagrate
         }
     }
 
