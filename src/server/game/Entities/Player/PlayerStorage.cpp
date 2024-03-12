43b2b6dc54221919703b0185feff1f7ff231f002
laasker <79991768+laasker@users.noreply.github.com>
2024-03-12 13:02:48 -0300

    (playerstorage.cpp) Futuro fix para floods Talent Archivist

--- src/server/game/Entities/Player/PlayerStorage.cpp
+++ src/server/game/Entities/Player/PlayerStorage.cpp
@@ -5750,6 +5750,7 @@ void Player::_LoadActions(PreparedQueryResult result)
             else
             {
                 LOG_ERROR("entities.player", "ActionButton loading problem, will be deleted from db...");
+                // Caso futuramente tiver muito flood no log por causa do Talent Archivist, talvez é melhor remover esse log_error
 
                 // Will deleted in DB at next save (it can create data until save but marked as deleted)
                 m_actionButtons[button].uState = ACTIONBUTTON_DELETED;
